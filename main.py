aeca450525b69ebb206ec4e3d453fd60246a1c2c
Gordon M <gordon62105@gmail.com>
2023-11-24 16:34:45 -0500

    added bg and movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Flappy Bird
 #Gordon Mo, Jacky Wong, Sara Perera, Selina Cheng
 
+#import libraries
 import pygame, random
 fsp = 60
 
@@ -15,8 +16,12 @@ from pygame.locals import (
     QUIT,
 )
 
+#set screen dimensions
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
+
+#load background 
+bg = pygame.image.load("background.png")
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
@@ -27,6 +32,17 @@ class Bird(pygame.sprite.Sprite):
             center=(
                 SCREEN_WIDTH/2-100, SCREEN_HEIGHT/2
             ))
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)    
+
+    #keep player on screen
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
 
 #initialize pygame
 pygame.init()
@@ -45,10 +61,21 @@ bird = Bird()
 #our main loop 
 while running:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == KEYDOWN:
+            #if escape key is pressed, quit game
+            if event.key == K_ESCAPE:
+                running = False
+        #exiting window, quits game
+        elif event.type == QUIT:
             running = False
-    screen.fill((255,255,255))
+    #screen.fill((255,255,255))
 
+    #get the set of keys that were pressed
+    pressed_keys = pygame.key.get_pressed()
+    #update bird's location
+    bird.update(pressed_keys)
+
+    screen.blit(bg,(0,0))
     screen.blit(bird.surf, bird.rect)
     #flip everything to the display
     pygame.display.flip()
